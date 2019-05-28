e8895a55d6325cdd96c175d5a149a3611256a467
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-05-28 17:51:50 +0200

    Exercise domain object and DAO

--- Admin Console/src/Main.java
+++ Admin Console/src/Main.java
@@ -1,5 +1,31 @@
+import DAO.ExerciseDao;
+import Domain.Exercise;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test Admin console");
+        Exercise exercise = new Exercise("Test Exercise DAO","Test Exercise DAO Desc.");
+
+        ExerciseDao exerciseDao = new ExerciseDao();
+
+        int id = exerciseDao.create(exercise).getId();
+
+        System.out.println("New exercise id: "+id);
+
+        Exercise createdExercise = exerciseDao.read(id);
+
+        System.out.println("New excercise title: "+createdExercise.getTitle());
+        System.out.println("New excercise description: "+createdExercise.getDescription());
+
+        createdExercise.setTitle("Test Exercise DAO 2");
+        createdExercise.setDescription("Test Exercise DAO Desc. 2");
+
+        exerciseDao.update(createdExercise);
+
+        createdExercise = exerciseDao.read(id);
+
+        System.out.println("New excercise title after update: "+createdExercise.getTitle());
+        System.out.println("New excercise description after update: "+createdExercise.getDescription());
+
+        exerciseDao.delete(id);
     }
 }
---
+++ Backend/src/DAO/ExerciseDao.java
@@ -0,0 +1,74 @@
+package DAO;
+
+import Domain.Exercise;
+import Domain.User;
+
+import java.sql.*;
+
+public class ExerciseDao extends BaseDao {
+
+    private static final String CREATE_EXERCISE_QUERY = "insert into exercise(title, description) VALUES (?,?);";
+    private static final String READ_EXERCISE_QUERY = "SELECT * FROM exercise where id = ?";
+    private static final String UPDATE_EXERCISE_QUERY = "UPDATE exercise SET title = ?, description = ? where id = ?";
+    private static final String DELETE_EXERCISE_QUERY = "DELETE FROM exercise WHERE id = ?";
+
+    public Exercise create(Exercise exercise) {
+        try (Connection conn = dbUtils.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_EXERCISE_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, exercise.getTitle());
+            statement.setString(2, exercise.getDescription());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                exercise.setId(resultSet.getInt(1));
+            }
+            return exercise;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Exercise read(int exerciseId) {
+        try (Connection conn = dbUtils.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_EXERCISE_QUERY);
+            statement.setInt(1, exerciseId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Exercise exercise = new Exercise();
+                exercise.setId(resultSet.getInt("id"));
+                exercise.setTitle(resultSet.getString("title"));
+                exercise.setDescription(resultSet.getString("description"));
+                return exercise;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(Exercise exercise) {
+        try (Connection conn = dbUtils.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_EXERCISE_QUERY);
+            statement.setString(1, exercise.getTitle());
+            statement.setString(2, exercise.getDescription());
+            statement.setInt(3, exercise.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int exerciseId) {
+        try (Connection conn = dbUtils.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_EXERCISE_QUERY);
+            statement.setInt(1, exerciseId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}

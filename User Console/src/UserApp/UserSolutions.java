cff30d8a9e5d896d430331ee5a7467398d5b8de5
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-06-03 21:07:02 +0200

    UserSolution module part II

--- User Console/src/UserApp/UserSolutions.java
+++ User Console/src/UserApp/UserSolutions.java
@@ -8,8 +8,7 @@ import Domain.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static Commons.ConsoleUtils.getInt;
-import static Commons.ConsoleUtils.printOptions;
+import static Commons.ConsoleUtils.*;
 
 public class UserSolutions {
 
@@ -65,6 +64,19 @@ public class UserSolutions {
             Solution solution = solutionDao.findSolutionForHomeworkAndUser(contextUser,homework);
             if(solution==null){
                 solution = new Solution();
+                solution.setCreated(LocalDateTime.now());
+                solution.setUpdated(LocalDateTime.now());
+                solution.setHomework_id(homework.getId());
+                solution.setUser_id(contextUser.getId());
+                System.out.println("Solution (link to onedrive): ");
+                String solutionDescription = getString();
+                solution.setDescription(solutionDescription);
+                solution = solutionDao.create(solution);
+                if(solution!=null){
+                    System.out.println("Solution save with id: "+solution.getId());
+                }else {
+                    System.out.println("Fail to save solution");
+                }
             }else {
                 System.out.println("Solution for this homework already exist and has id: "+solution.getId());
             }
@@ -79,6 +91,20 @@ public class UserSolutions {
     }
 
     private void updateSolution() {
+        System.out.println("Provide solution id: ");
+        int solutionId = getInt();
+        Solution solution = solutionDao.read(solutionId);
+        if(solution!=null){
+            System.out.println("Old solution description: " + solution.getDescription() + " New description: ");
+            String newDescription = getString();
+            solution.setDescription(newDescription);
+            solution.setUpdated(LocalDateTime.now());
+            solutionDao.update(solution);
+            System.out.println("Solution updated");
+        }else {
+            System.out.println("There is no solution with id: " + solutionId);
+        }
+
     }
 
     private void printHomeworkWithExercise(Homework homework) {
